73926cb46f290e07f1e23c24cbcf599aaed88a65
muchluvu <rorrxr@naver.com>
2024-11-16 13:48:11 +0900

    관심 상품 조회 API 구현

--- src/main/java/com/sparta/myselectshop/controller/ProductController.java
+++ src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -1,75 +1,42 @@
-//package com.sparta.myselectshop.controller;
-//
-//import com.sparta.myselectshop.dto.ProductRequestDto;
-//import com.sparta.myselectshop.dto.ProductResponseDto;
-//import com.sparta.myselectshop.security.UserDetailsImpl;
-//import com.sparta.myselectshop.service.ProductService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.data.domain.Page;
-//import org.springframework.security.core.annotation.AuthenticationPrincipal;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@Controller
-//public class ProductController {
-//
-//    @Autowired
-//    private ProductService productService;
-//
-//    // 관심 상품 등록하기
-////    @PostMapping("/products")
-////    public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto) {
-////        // 응답 보내기
-////        return productService.createProduct(requestDto);
-////    }
-//
-//    // 관리자 조회
-//    @GetMapping("/admin/products")
-//    public List<ProductResponseDto> getAllProducts() {
-//        return productService.getAllProducts();
+package com.sparta.myselectshop.controller;
+
+import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
+import com.sparta.myselectshop.dto.ProductRequestDto;
+import com.sparta.myselectshop.dto.ProductResponseDto;
+import com.sparta.myselectshop.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/api")
+@RequiredArgsConstructor
+@RestController
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    // 관심 상품 등록하기
+    @PostMapping("/products")
+    public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto) {
+        // 응답 보내기
+        return productService.createProduct(requestDto);
+    }
+
+//    // 관심 상품 희망 최저가 등록하기
+//    @PutMapping("/products/{id}")
+//    public ProductResponseDto updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) {
+//        // 응답 보내기
+//        return productService.updateProduct(id, requestDto);
 //    }
+//
 //    // 관심 상품 조회하기
 //    @GetMapping("/products")
-//    public Page<ProductResponseDto> getProducts(
-//            @RequestParam("page") int page,
-//            @RequestParam("size") int size,
-//            @RequestParam("sortBy") String sortBy,
-//            @RequestParam("isAsc") boolean isAsc,
-//            @AuthenticationPrincipal UserDetailsImpl userDetails) {
+//    public List<ProductResponseDto> getProducts() {
 //        // 응답 보내기
-//        return productService.getProducts(userDetails.getUser(),  page-1, size, sortBy, isAsc);
+//        return productService.getProducts();
 //    }
-//
-//    // 상품에 폴더 추가
-//    @PostMapping("/products/{productId}/folder")
-//    public void addFolder(
-//            @PathVariable Long productId,
-//            @RequestParam Long folderId,
-//            @AuthenticationPrincipal UserDetailsImpl userDetails
-//    ) {
-//        productService.addFolder(productId, folderId, userDetails.getUser());
-//    }
-//
-//    // 회원이 등록한 폴더 내 모든 상품 조회
-//    @GetMapping("/folders/{folderId}/products")
-//    public Page<ProductResponseDto> getProductsInFolder(
-//            @PathVariable Long folderId,
-//            @RequestParam int page,
-//            @RequestParam int size,
-//            @RequestParam String sortBy,
-//            @RequestParam boolean isAsc,
-//            @AuthenticationPrincipal UserDetailsImpl userDetails
-//    ) {
-//        return productService.getProductsInFolder(
-//                folderId,
-//                page-1,
-//                size,
-//                sortBy,
-//                isAsc,
-//                userDetails.getUser()
-//        );
-//    }
-//
-//}
+
+}

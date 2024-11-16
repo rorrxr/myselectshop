f906888765599abf11ab751a70480e96ed77de5d
muchluvu <rorrxr@naver.com>
2024-11-16 15:37:19 +0900

    관심 상품 희망 최저가 업데이트 API

--- src/main/java/com/sparta/myselectshop/controller/ProductController.java
+++ src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -25,18 +25,18 @@ public class ProductController {
         return productService.createProduct(requestDto);
     }
 
-//    // 관심 상품 희망 최저가 등록하기
-//    @PutMapping("/products/{id}")
-//    public ProductResponseDto updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) {
-//        // 응답 보내기
-//        return productService.updateProduct(id, requestDto);
-//    }
-//
-//    // 관심 상품 조회하기
-//    @GetMapping("/products")
-//    public List<ProductResponseDto> getProducts() {
-//        // 응답 보내기
-//        return productService.getProducts();
-//    }
+    // 관심 상품 희망 최저가 등록하기
+    @PutMapping("/products/{id}")
+    public ProductResponseDto updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) {
+        // 응답 보내기
+        return productService.updateProduct(id, requestDto);
+    }
+
+    // 관심 상품 조회하기
+    @GetMapping("/products")
+    public List<ProductResponseDto> getProducts() {
+        // 응답 보내기
+        return productService.getProducts();
+    }
 
 }
--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -26,31 +26,31 @@ public class ProductService {
         return new ProductResponseDto(product);
     }
 
-//    @Transactional
-//    public ProductResponseDto updateProduct(Long id, ProductMypriceRequestDto requestDto) {
-//        int myprice = requestDto.getMyprice();
-//        if (myprice < MIN_MY_PRICE) {
-//            throw new IllegalArgumentException("유효하지 않은 관심 가격입니다. 최소 " + MIN_MY_PRICE + " 원 이상으로 설정해 주세요.");
-//        }
-//
-//        Product product = productRepository.findById(id).orElseThrow(() ->
-//                new NullPointerException("해당 상품을 찾을 수 없습니다.")
-//        );
-//
-//        product.update(requestDto);
-//
-//        return new ProductResponseDto(product);
-//    }
-//
-//    public List<ProductResponseDto> getProducts() {
-//        List<Product> productList = productRepository.findAll();
-//        List<ProductResponseDto> responseDtoList = new ArrayList<>();
-//
-//        for (Product product : productList) {
-//            responseDtoList.add(new ProductResponseDto(product));
-//        }
-//        return responseDtoList;
-//    }
+    @Transactional
+    public ProductResponseDto updateProduct(Long id, ProductMypriceRequestDto requestDto) {
+        int myprice = requestDto.getMyprice();
+        if (myprice < MIN_MY_PRICE) {
+            throw new IllegalArgumentException("유효하지 않은 관심 가격입니다. 최소 " + MIN_MY_PRICE + " 원 이상으로 설정해 주세요.");
+        }
+
+        Product product = productRepository.findById(id).orElseThrow(() ->
+                new NullPointerException("해당 상품을 찾을 수 없습니다.")
+        );
+
+        product.update(requestDto);
+
+        return new ProductResponseDto(product);
+    }
+
+    public List<ProductResponseDto> getProducts() {
+        List<Product> productList = productRepository.findAll();
+        List<ProductResponseDto> responseDtoList = new ArrayList<>();
+
+        for (Product product : productList) {
+            responseDtoList.add(new ProductResponseDto(product));
+        }
+        return responseDtoList;
+    }
 //
 //    @Transactional
 //    public void updateBySearch(Long id, ItemDto itemDto) {
